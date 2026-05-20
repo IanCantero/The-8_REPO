7d1464274d5340dc77abf2211ffe71218089dba9
IanCantero <136346391+IanCantero@users.noreply.github.com>
2026-05-21 00:00:46 +0200

    Abuela AI

---
+++ The 8/Assets/_Root_The8/Scripts/GameWork/GrannyAIBase.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GrannyAIBase : MonoBehaviour
+{
+
+    public Transform[] walkpoints;
+
+    NavMeshAgent agent;
+    Animator anim;
+
+    private enum State
+    {
+        Sitting,
+        StandingUp,
+        Walking,
+        Idle
+    }
+
+    State currentState = State.Sitting;
+    int currentWalkpointIndex = 0;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        //Asunsion quieta
+        agent.isStopped = true; 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartSequence();
+        }
+    }
+
+    // Llamar para empezar
+    public void StartSequence()
+    {
+        if (currentState == State.Sitting)
+        {
+            currentState = State.StandingUp;
+            anim.SetTrigger("StandUp");
+        }
+    }
+
+    // Este método puedes llamarlo desde un Animation Event
+    public void OnStandUpFinished()
+    {
+        currentState = State.Walking;
+        agent.isStopped = false;
+        agent.SetDestination(walkpoints[currentWalkpointIndex].position);
+
+        anim.SetBool("isWalking", true);
+    }
+
+    void Update()
+    {
+        if (currentState == State.Walking)
+        {
+            if (!agent.pathPending && agent.remainingDistance < 0.2f)
+            {
+                GoToNextWaypoint();
+            }
+        }
+    }
+
+    void GoToNextWaypoint()
+    {
+
+        currentWalkpointIndex++;
+        if (currentWalkpointIndex < walkpoints.Length)
+        {
+            agent.SetDestination(walkpoints[currentWalkpointIndex].position);
+        }
+        else
+        {
+            currentState = State.Idle;
+            agent.isStopped = true;
+
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
